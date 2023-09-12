e2a9908a25b17a1cb2a834fbafe308b97778fbdb
miin <busster@naver.com>
2023-09-13 00:56:10 +0900

    AddBottomNavigationView

--- app/src/main/java/exercise/used_goods_transaction/MainActivity.kt
+++ app/src/main/java/exercise/used_goods_transaction/MainActivity.kt
@@ -2,6 +2,8 @@ package exercise.used_goods_transaction
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import exercise.used_goods_transaction.chatList.ChatListFragment
 import exercise.used_goods_transaction.home.HomeFragment
 import exercise.used_goods_transaction.myPage.MyPageFragment
@@ -19,10 +21,31 @@ class MainActivity : AppCompatActivity() {
         val chatListFragment = ChatListFragment()
         val myPageFragment = MyPageFragment()
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottmNavi)
 
+        replaceFragment(homeFragment)
 
+        // 바텀 내비 안에 있는 아이템을 눌렀을때 그 프래그먼트로 대체하면 됨
 
+        bottomNavigationView.setOnNavigationItemSelectedListener {
 
+            when(it.itemId){
+                R.id.home-> replaceFragment(homeFragment)
+                R.id.chat->replaceFragment(chatListFragment)
+                R.id.myPage->replaceFragment(myPageFragment)
+
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        supportFragmentManager.beginTransaction().apply{
+            replace(R.id.frameLayout,fragment)
+            commit()
+        }
 
     }
 }
